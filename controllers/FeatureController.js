f33043c4f3c353501ec4e5081854257d5a3cc27b
niranjan65 <niranjansinghhh16@gmail.com>
2024-05-18 09:11:31 +0530

    added feature controller

--- controllers/FeatureController.js
+++ controllers/FeatureController.js
@@ -1,5 +1,7 @@
+import { url } from "inspector";
 import { Food } from "../models/Food";
 import { User } from "../models/User";
+import Stripe from "stripe";
 
 
 // ADD TO CART
@@ -93,9 +95,144 @@ const removeFromCart = async (req, res) => {
             return res.status(400).json({success: false, message: "Food not found"})
         }
 
-        return res.status(200).json({success: true, message: "Removed from cart"})
+        return res.status(200).json({success: true, message: "Food Removed from cart"})
 
     } catch (error) {
         return res.status(500).json({success: false, message: error.message})
     }
+}
+
+// INCREMENT QUANTITY ROUTE
+const incrementQuantity = async (req, res) => {
+
+    const id = req.params.id;
+
+    try {
+        
+        let food = await Food.findOneAndUpdate(
+          { _id: id },
+          {
+            $set: {
+              quantity: { $add: ["$quantity", 1] },
+              totalPrice: { $multiply: ["$price", { $add: ["$quantity", 1] }] },
+            },
+          },
+          {
+            upsert: true,
+            new: true
+          }
+        );
+
+        if(!food) {
+            return res
+               .status(400)
+               .json( {success: true, message: "Food quantity incresesd", food})
+        }
+
+    } catch (error) {
+        return res.status(500).json({success: false, message: error.message})
+    }
+}
+
+// DECREMENT QUANTITY ROUTE
+const decrementQuantity = async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        
+        let food = await Food.findOneAndUpdate({_id: id, quantity: { $gt: 0}}, {
+            $set: {
+                quantity: { $subtract: ["$quantity", 1]},
+                totalPrice: {$subtract: ["$totalPrice", "$price"]},
+            }
+        }, {
+            upsert: true,
+            new: true
+        })
+
+        if(!food) {
+            return res.status(400).json({success: false, message: "Food not found or the product is out of stock"})
+        }
+
+        return res 
+           .status(200)
+           .json({ success: true, message: "Food quantity decremented"})
+
+    } catch (error) {
+        return res.status(500).json({success: false, message: error.message})
+    }
+}
+
+// CLEAR CART ROUTE
+const clearCart = async (req, res) => {
+    const userId = req.id;
+
+    try {
+
+        const deletedItems = await Food.deleteMany({userId})
+        const deletedList = await User.findOneAndUpdate(
+            {_id: userId},
+            {
+                cartItems: [],
+            }
+        )
+
+        if(!deletedItems) {
+            return res
+               .status(400)
+               .json({success: false, message: "Failed to clear cart"})
+        }
+        
+        return res.status(200).json({success: true, message: "order confirmed"})
+    } catch (error) {
+        return res.status(500).json({success: false, message: error.message})
+    }
+}
+
+const stripe = new Stripe(process.env.STRIPE_KEY)
+// CHECKOUT ROUTE
+const checkOut = async (req, res) => {
+
+    const userId = req.id;
+
+    try {
+
+        const cartItems = await Food.find({userId});
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            mode: "payment",
+            line_items: cartItems.map((item) => {
+                return {
+                    price_data: {
+                        currency: "inr",
+                        product_data: {
+                            name: item.name,
+                            images: [item.image]
+                        },
+                        unit_amount: item.price * 100,
+                    },
+
+                    quantity: item.quantity,
+                }
+            }),
+            session_url: "http://localhost:5173/success",
+            cancel_url: "http://localhost:5173/"
+        })
+
+        return res.json({url: session.url})
+        
+    } catch (error) {
+        return res.status(500).json({success: false, message: error.message})
+    }
+}
+
+export {
+    addToCart,
+    getCart,
+    removeFromCart,
+    incrementQuantity,
+    decrementQuantity,
+    clearCart,
+    checkOut
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { getUser, login, logout, resetPassword, signup, verifyOtp } from "../controllers/AuthController.js";
 import { verifyToken } from "../middlewares/VerifyToken.js";
+import { addToCart, checkOut, clearCart, decrementQuantity, getCart, incrementQuantity, removeFromCart } from "../controllers/FeatureController.js";
 
 const router = Router()
 
@@ -11,5 +12,16 @@ router.get("/logout", logout)
 router.put("/reset-password", resetPassword)
 router.put("/verify-otp", verifyOtp)
 router.get("/get-user", verifyToken, getUser)
+
+//FEATURE ROUTES
+router.post("/add-to-cart/:id", addToCart)
+router.get("/get-cart/:id", getCart)
+router.delete("/remove-from-cart/:id", removeFromCart)
+router.put("/increment-quantity/:id", incrementQuantity)
+router.put("/decrement-quantity/:id", decrementQuantity)
+router.get("/checkout", verifyToken, checkOut)
+router.get("clear-cart", verifyToken, clearCart)
+
+
 
 export default router
